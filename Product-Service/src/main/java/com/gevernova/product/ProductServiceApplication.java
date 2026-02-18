742bd6d9b08f1b696a324a201ba5ba975b5a88b1
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-02-19 01:07:24 +0530

    [GurleenKaur]: implement product service with CRUD and stock validation

--- Product-Service/src/main/java/com/gevernova/product/ProductServiceApplication.java
+++ Product-Service/src/main/java/com/gevernova/product/ProductServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
