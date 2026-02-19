efd6fc7c8285a3ee6e7769d9c9b5c4b48b7391d8
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-02-19 21:58:06 +0530

    [GurleenKaur]: Added GlobalExceptionHandler to handle exceptions

---
+++ Order-Service/src/main/java/com/gevernova/order/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package org.gevernova.order.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<Object> handleOrderNotFound(OrderNotFoundException ex) {
+
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("message", ex.getMessage());
+        error.put("status", 404);
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
